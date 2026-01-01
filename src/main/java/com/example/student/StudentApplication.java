cd441419751c6d7d078df881cdbdd9ae4f6f2a4c
Abhijit Bonde <abhijitbonde01@gmail.com>
2026-01-01 15:07:43 +0530

    Initial commit

---
+++ src/main/java/com/example/student/StudentApplication.java
@@ -0,0 +1,26 @@
+
+package com.example.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Student Result App Running";
+    }
+
+    @GetMapping("/result")
+    public String result() {
+        return "{\"name\": \"Abhijit\", \"marks\": 85}";
+    }
+}
